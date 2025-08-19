73c121bfb1b1f4a071a00d9a10d50d1614fe29e4
Aliza Khan <alizaadi2@gmail.com>
2025-08-19 21:01:46 +0500

    Created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { NAV_LINKS } from "@/app/constants";
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <header className="bg-slate-200 shadow-md">
+
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link href="/">
+                    <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                        <span className="text-green-600">Khan</span>
+                        <span className="text-green-900">Estate</span>
+                    </h1>
+                </Link>
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input type="text" placeholder="Search here" className="bg-transparent focus:outline-none w-24 sm:w-64" />
+                    <button>
+                        <p>search</p>
+                        {/* <FaSearch className="text-slate-600"/> */}
+                    </button>
+                </form>
+                <ul className="flex gap-4">
+                    {
+                        NAV_LINKS.map((link) => {
+                            return <Link href={link.href} key={link.key}><li className="hidden md:inline text-slate-700 hover:underline">{link.label}</li></Link>
+                        })
+                    }
+                </ul>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
