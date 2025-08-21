49f8c1c3dd7cb25b2baf02646ed0159ca19920f3
Aliza Khan <alizaadi2@gmail.com>
2025-08-21 20:26:01 +0500

    updated field

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -17,7 +17,7 @@ export const createOrUpdateUser = async (
                     firstName: first_name,
                     lastName: last_name,
                     profilePicture: image_url,
-                    email: email_addresses[0].email_addresses,
+                    email: email_addresses[0].email_address,
                 },
             }, { upsert: true, new: true }
         )
